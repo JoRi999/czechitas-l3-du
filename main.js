c5304aa0386e7dd79ab9e77105c2af44c19bf823
JoRi999 <ringelovaj@gmail.com>
2023-02-08 17:10:38 +0100

    Řešení DÚ

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 // tady je místo pro náš program
 
+let jmeno = prompt("Zadejte Vaše křestní jméno: ");
 
+let prijmeni = prompt("Zadejte Vaše příjmení: ");
 
+let rok = Number(prompt("Zadejte rok Vašeho narození: "));
 
+let vek = (2023 - rok)
+
+let vystup = document.querySelector(".vystupProgramu")
+
+vystup.innerHTML = "Zde je Vaše jméno a věk: " + jmeno + " " + prijmeni + ", " + vek + " let"
+
+vystup.style.fontWeight = "bold"
+
+function barva(){
+    	let oblibenaBarva = prompt("Jaká je Vaše oblíbená barva (anglicky): ")
+        vystup.style.color = oblibenaBarva
+
+}
